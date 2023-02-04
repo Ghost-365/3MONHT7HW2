109d922523709dc39f80ef94fd1e7c079df9ca8c
Ghost-365 <akzoltoevazat94@gmail.com>
2023-02-04 19:01:26 +0600

    GIGHIU

---
+++ app/src/main/java/com/example/MONTH3HW7/SecondFragment.kt
@@ -0,0 +1,32 @@
+package com.example.MONTH3HW7
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.MONTH3HW7.FirstFragment.Companion.KEY_DYNAMIC_ARGS
+import com.example.MONTH3HW7.databinding.FragmentSecondBinding
+
+@Suppress("DEPRECATION")
+class SecondFragment : Fragment() {
+    private lateinit var binding: FragmentSecondBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentSecondBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val result = arguments?.getSerializable(KEY_DYNAMIC_ARGS) as Character
+        binding.apply {
+            ivImage.loadImage(result.image.toString())
+            tvStatus.text = result.status
+            tvName.text = result.name
+        }
+    }
+
+}
